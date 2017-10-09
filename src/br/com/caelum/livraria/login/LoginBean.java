bf2cb488429914092d07360e48993882955b6da7
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-08 23:28:33 -0300

    Injetando DAO como dependência.

--- src/br/com/caelum/livraria/login/LoginBean.java
+++ src/br/com/caelum/livraria/login/LoginBean.java
@@ -11,50 +11,52 @@ import br.com.caelum.livraria.modelo.Usuario;
 
 @Model
 public class LoginBean {
-	
-	private Usuario usuario = new Usuario();
-	private UsuarioDao dao = new UsuarioDao();
-	
-	@Inject
-	UsuarioLogadoBean usuarioLogado;
-	
-	@Inject
-	MenuBean menu;
 
-	public Usuario getUsuario() {
-		return usuario;
-	}
-	
-	public String efetuaLogin() {
-		
-		Usuario usuarioEncontrado = this.dao.buscaPeloLogin(usuario.getLogin());
-		
-		if(usuarioEncontrado!= null && possuiMesmaSenha(usuarioEncontrado)) {
-			usuarioLogado.logar(usuarioEncontrado);
-			return menu.paginaLivros();
-		}
-		
-		criaMensagem("Usuário não encontrado!");
-		limparForm();
-		
-		return "";
-	}
-	
-	public String efetuaLogout() {
-		this.usuarioLogado.deslogar();
-		return this.menu.paginaLogin();
-	}
+    private Usuario usuario = new Usuario();
 
-	
-	private void limparForm() {
-		this.usuario = new Usuario();
-	}
+    @Inject
+    private UsuarioDao dao;
 
-	private void criaMensagem(String mensagem) {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, ""));
-	}
+    @Inject
+    UsuarioLogadoBean usuarioLogado;
 
-	private boolean possuiMesmaSenha(Usuario usuarioEncontrado) {
-		return usuarioEncontrado.getSenha().equals(usuario.getSenha());
-	}
+    @Inject
+    MenuBean menu;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public String efetuaLogin() {
+
+        Usuario usuarioEncontrado = this.dao.buscaPeloLogin(usuario.getLogin());
+
+        if (usuarioEncontrado != null && possuiMesmaSenha(usuarioEncontrado)) {
+            usuarioLogado.logar(usuarioEncontrado);
+            return menu.paginaLivros();
+        }
+
+        criaMensagem("Usuário não encontrado!");
+        limparForm();
+
+        return "";
+    }
+
+    public String efetuaLogout() {
+        this.usuarioLogado.deslogar();
+        return this.menu.paginaLogin();
+    }
+
+
+    private void limparForm() {
+        this.usuario = new Usuario();
+    }
+
+    private void criaMensagem(String mensagem) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, ""));
+    }
+
+    private boolean possuiMesmaSenha(Usuario usuarioEncontrado) {
+        return usuarioEncontrado.getSenha().equals(usuario.getSenha());
+    }
 }

a8daa63df880bdd02cf63856368a3bbd85380bed
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-14 18:11:23 -0300

    Migrando banco simulado em memória para MySQL com JPA.

--- src/br/com/caelum/livraria/dao/LivroDao.java
+++ src/br/com/caelum/livraria/dao/LivroDao.java
@@ -5,20 +5,21 @@ import java.util.List;
 import br.com.caelum.livraria.modelo.Livro;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class LivroDao {
 
-    @Inject
-    private Banco banco;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public void salva(Livro livro) {
-        banco.save(livro);
+        entityManager.persist(livro);
     }
 
     public List<Livro> todosLivros() {
-        return banco.listaLivros();
+        return entityManager.createQuery("select l from Livro l", Livro.class).getResultList();
     }
 
 }
--- src/br/com/caelum/livraria/dao/UsuarioDao.java
+++ src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -3,16 +3,21 @@ package br.com.caelum.livraria.dao;
 import br.com.caelum.livraria.modelo.Usuario;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class UsuarioDao {
 
-    @Inject
-    private Banco banco;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public Usuario buscaPeloLogin(String login) {
-        return this.banco.buscaPeloNome(login);
+
+        return (Usuario) entityManager
+                .createQuery("select u from Usuario u where u.login = :pLogin")
+                .setParameter("pLogin", login)
+                .getSingleResult();
     }
 
 }
